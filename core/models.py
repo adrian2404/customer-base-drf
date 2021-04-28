e8cfc90d46b327b63f32762b723613f8e3e89710
adrian2404 <adrian.yavorski@gmail.com>
2021-04-28 20:47:33 +0300

    Create customer endpoint

--- core/models.py
+++ core/models.py
@@ -5,10 +5,16 @@ from django.db import models
 class Profession(models.Model):
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.description
+
 
 class DataSheet(models.Model):
     description = models.CharField(max_length=100)
     historical_data = models.TextField()
+
+    def __str__(self):
+        return self.description
 
 
 class Customer(models.Model):
