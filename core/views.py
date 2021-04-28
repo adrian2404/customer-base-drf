1ebec87fd25a38eee78bfef6b4d45556e7c3c6d4
adrian2404 <adrian.yavorski@gmail.com>
2021-04-28 23:32:02 +0300

    Hands on with framework
    Practice with redefining some common methods
    Added some custom actions

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .models import Customer, Profession, DataSheet, Document
 from .serializers import (
@@ -14,6 +16,95 @@ class CustomerViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         active_customers = Customer.objects.filter(active=True)
         return active_customers
+
+    def list(self, request, *args, **kwargs):
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        obj = self.get_object()
+        serializer = CustomerSerializer(obj)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        customer = Customer.objects.create(
+            name=data['name'], address=data['address'], data_sheet_id=data['data_sheet']
+        )
+        profession = Profession.objects.get(id=data['professions'])
+        customer.professions.add(profession)
+        customer.save()
+
+        serializer = CustomerSerializer(customer)
+
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        data = request.data
+        customer = self.get_object()
+        customer.name = data['name']
+        customer.address = data['address']
+        customer.data_sheet_id = data['data_sheet']
+
+        profession = Profession.objects.get(id=data['professions'])
+        for p in customer.professions.all():
+            customer.professions.remove(p)
+        customer.professions.add(profession)
+        customer.save()
+
+        serializer = CustomerSerializer(customer)
+
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        customer = self.get_object()
+        data = request.data
+        customer.name = data.get('name', customer.name)
+        customer.address = data.get('address', customer.address)
+        customer.data_sheet_id = data.get('data_sheet', customer.data_sheet_id)
+        customer.save()
+
+        serializer = CustomerSerializer(customer)
+
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        customer = self.get_object()
+        customer.delete()
+
+        return Response('Object removed')
+
+    @action(detail=True)
+    def deactivate(self, request, **kwargs):
+        customer = self.get_object()
+        customer.active = False
+        customer.save()
+        serializer = CustomerSerializer(customer)
+
+        return Response(serializer.data)
+
+    @action(detail=False)
+    def deactivate_all(self, request, **kwargs):
+        customers = Customer.objects.all()
+        customers.update(active=False)
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False)
+    def activate_all(self, request, **kwargs):
+        customers = Customer.objects.all()
+        customers.update(active=True)
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['POST'])
+    def change_status(self, request, **kwargs):
+        status = True if request.data.get('active') == 'True' else False
+        customers = Customer.objects.all()
+        customers.update(active=status)
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
 
 
 class ProfessionViewSet(viewsets.ModelViewSet):
