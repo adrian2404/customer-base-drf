9e6b0f89888f460e67560eee8aecd364058deccd
adrian2404 <adrian.yavorski@gmail.com>
2021-05-19 13:49:15 +0300

    Removed unused stuff

--- customer_base/settings.py
+++ customer_base/settings.py
@@ -1,5 +1,4 @@
 import os
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -52,18 +51,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'customer_base.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'customer_base',
-        'PASSWORD': 'django1234',
-        'USER': 'adrian',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
